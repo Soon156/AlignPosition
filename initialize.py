85fc4c54f737582ddeaa0b15dfa75d460020e464
Soon156 <soonkokseng2015@gmail.com>
2023-02-15 07:53:49 -0800

    add exception when camera not available

--- initialize.py
+++ initialize.py
@@ -23,14 +23,13 @@ default_value = {'Width': 63,
 
 # get camera list
 def get_available_cameras():
-    devices = FilterGraph().get_input_devices()
-
     available_cameras = {}
-
-    for device_index, device_name in enumerate(devices):
-        available_cameras[device_index] = device_name
-
-    return available_cameras
+    try:
+        devices = FilterGraph().get_input_devices()
+        for device_index, device_name in enumerate(devices):
+            available_cameras[device_index] = device_name
+    finally:
+        return available_cameras
 
 
 # save value to config
