cd543b62a407575c369f43a5d0cb8ad99ed12383
Soon156 <soonkokseng2015@gmail.com>
2023-05-05 15:52:32 +0800

    posture recognize ver1

--- PositionDetect.py
+++ PositionDetect.py
@@ -1,72 +1,68 @@
 import cv2
 import mediapipe as mp
-from Config import append_landmark
-saved_pose = []
+import numpy as np
 
 mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
-pose = mpPose.Pose(model_complexity=1)
-
+pose = mpPose.Pose(model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+threshold = 0.1
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
 
 while True:
     success, img = cap.read()
     if not success:
+        print("Failed to read frame")
         break
 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
 
-    if results.pose_landmarks:
-        saved_pose = results.pose_landmarks.landmark
-        '''
-        # get the landmarks of the left and right eyes
-        left_eye_landmarks = results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_EYE] if results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_EYE] else None
-        right_eye_landmarks = results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_EYE] if results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_EYE] else None
-        '''
-
-        '''
-        if left_eye_landmarks:
-            # print the coordinates of the left eye landmarks
-            print("Left eye landmarks:")
-            print(f"x={left_eye_landmarks.x}, y={left_eye_landmarks.y}, z={left_eye_landmarks.z}")
-        else:
-            print("Left eye not detected")
-
-        if right_eye_landmarks:
-            # print the coordinates of the right eye landmarks
-            print("Right eye landmarks:")
-            print(f"x={right_eye_landmarks.x}, y={right_eye_landmarks.y}, z={right_eye_landmarks.z}")
-        else:
-            print("Right eye not detected")
-        '''
-
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) == ord('q'):
-        ret, frame = cap.read()
-        append_landmark(saved_pose, frame)
-
+    # Display the image with the landmark annotations
+    annotated_image = img.copy()
+    mp.solutions.drawing_utils.draw_landmarks(annotated_image, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+    cv2.imshow("Posture Detection", annotated_image)
+    key = cv2.waitKey(1)
+    # Break the loop if the user presses the "q" key
+    if key == ord("q"):
         break
 
-cv2.destroyAllWindows()
+    if key == ord("c"):
+        # Detect the posture landmarks using Mediapipe
+        results = pose.process(img)
 
-'''
-# Detect Position
-def position_detect():
-    global object_distance
-    global counter_position
-    values = i.get_val()
-    try:
-        ds = float(values.get('distance'))
-        r = float(values.get('range'))
-        if object_distance < (ds - r):
-            if counter_position >= float(values.get('position')):
-                # active_notification(2)
-                counter_position = 0
+        # If landmarks were detected, compare them to the saved landmarks
+        if results.pose_landmarks:
+            # Load the saved landmark data from file
+            with open("landmarks.npy", "rb") as f:
+                saved_landmarks = np.load(f)
+
+            # Extract the landmark data as a numpy array
+            new_landmarks = np.array(
+                [[lmk.x, lmk.y, lmk.z, lmk.visibility] for lmk in results.pose_landmarks.landmark])
+
+            # Compute the mean absolute error between the new landmarks and the saved landmarks
+            mean_error = np.mean(np.abs(new_landmarks - saved_landmarks))
+
+            # Print whether the new posture is the same as the saved posture
+            if mean_error < threshold:
+                print("The posture is the same as the saved posture")
             else:
-                counter_position += 1
+                print("The posture is different from the saved posture")
+
+    if key == ord("s"):
+        # If landmarks were detected, save them to a file
+        if results.pose_landmarks:
+            # Extract the landmark data as a numpy array
+            landmark_data = np.array(
+                [[lmk.x, lmk.y, lmk.z, lmk.visibility] for lmk in results.pose_landmarks.landmark])
+
+            # Save the landmark data to a file
+            with open("landmarks.npy", "wb") as f:
+                np.save(f, landmark_data)
+                print("Saved")
         else:
-            counter_position = 0
-    except ValueError:
-        pass
-        '''
+            print("Failed to save")
+
+cap.release()
+cv2.destroyAllWindows()
