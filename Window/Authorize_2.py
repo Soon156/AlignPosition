1cf468ca49494f235428009b054233c42389e820
soonk <soonkokseng2015@gmail.com>
2023-08-22 02:43:39 +0800

    fix GUI and parental control

--- Window/Authorize_2.py
+++ Window/Authorize_2.py
@@ -1,33 +1,24 @@
-from PySide6.QtWidgets import QDialog
-import logging as log
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import QDialog, QLineEdit
 from ParentalControl.Auth import login_user
 from Window.ui_Authorize import Ui_PINDialog
 
-cond_valid = True
-
-
-def change_condition():
-    global cond_valid
-    cond_valid = False
-
-
-def get_condition():
-    return cond_valid
-
 
 class PINDialog2(QDialog, Ui_PINDialog):
+    finished = Signal()
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.PIN_line.returnPressed.connect(self.PIN_btn.click)
+        self.PIN_line.setEchoMode(QLineEdit.Password)
+        self.PIN_hint_lbl.hide()
+        self.PIN_btn.clicked.connect(self.valid_pin)
 
     def valid_pin(self):
         if login_user(self.PIN_line.text()):
             self.close()
-            change_condition()
-            log.info("Cancel Sleeping")
-
+            self.finished.emit()
         else:
             self.PIN_hint_lbl.setText("Incorrect PIN")
             self.PIN_hint_lbl.show()
