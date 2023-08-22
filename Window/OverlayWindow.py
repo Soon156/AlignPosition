246296277f9697f67777615f533605352b8a87ae
soonk <soonkokseng2015@gmail.com>
2023-08-22 18:44:34 +0800

    new overlay logo and logic

--- Window/OverlayWindow.py
+++ Window/OverlayWindow.py
@@ -1,23 +1,17 @@
-import sys
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
-from PySide6.QtGui import QPixmap
-from PySide6.QtCore import Qt, QRect, QPropertyAnimation, QParallelAnimationGroup, QEasingCurve
-import Window.resource_rc
-from Funtionality.Config import get_config
-
-image_path = u":/icon/eyes.png"
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PySide6.QtGui import QPixmap, QTransform
+from PySide6.QtCore import Qt, QRect, QPropertyAnimation, QParallelAnimationGroup, QEasingCurve, QSize
+from Funtionality.Config import get_config, abs_overlay_pic_path
 
 
 class OverlayWidget(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.width_geo, self.pos = self.win_geometry()
-        if self.pos == "left":
-            self.setGeometry(-30, -50, 100, 100)
-        else:
-            self.setGeometry(self.width_geo - 80, -50, 100, 100)
 
+        self.width_geo = None
+        self.pos = None
+        self.pixmap = None
         self.setWindowFlags(
             Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint | Qt.WindowTransparentForInput | Qt.SplashScreen
         )
@@ -25,14 +19,17 @@ class OverlayWidget(QWidget):
         self.setWindowOpacity(0.5)
         self.image_label = QLabel(self)
         self.image_label.setAlignment(Qt.AlignCenter)
-        pixmap = QPixmap(image_path)
-        pixmap = pixmap.scaled(self.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        self.image_label.setPixmap(pixmap)
-        self.image_label.setGeometry(0, 0, self.width(), self.height())
+
+        pixmap = QPixmap(abs_overlay_pic_path)
+        pixmap = pixmap.scaled(QSize(100, 100), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.original_pixmap = pixmap.copy()
+        self.win_geometry()
+
         layout = QVBoxLayout()
         layout.addWidget(self.image_label)
         self.setLayout(layout)
         self.animation_group = QParallelAnimationGroup()
+
         self.state = False
 
     def show_with_animation(self, target_geometry, duration=300):
@@ -56,17 +53,17 @@ class OverlayWidget(QWidget):
     def change_state(self, posture):
         if posture == "bad":
             if self.pos == "left":
-                self.show_with_animation(QRect(0, -15, 100, 100))
+                self.show_with_animation(QRect(-15, -15, 100, 100))
             else:
-                self.show_with_animation(QRect(self.width_geo - 120, -15, 100, 100))
+                self.show_with_animation(QRect(self.width_geo - 90, -10, 100, 100))
             self.setWindowOpacity(1.0)
             self.state = True
         else:
             if self.state:
                 if self.pos == "left":
-                    self.show_with_animation(QRect(-30, -50, 100, 100))
+                    self.show_with_animation(QRect(-30, -30, 100, 100))
                 else:
-                    self.show_with_animation(QRect(self.width_geo - 80, -50, 100, 100))
+                    self.show_with_animation(QRect(self.width_geo - 80, -30, 100, 100))
                 self.state = False
                 self.setWindowOpacity(0.5)
 
@@ -75,11 +72,18 @@ class OverlayWidget(QWidget):
         if screen is not None:
             values = get_config()
             if values['overlay'] == "Left":
-                width = 0
-                pos = "left"
+                self.width_geo = 0
+                self.pos = "left"
             else:
-                width = screen.width()
-                pos = "right"
+                self.width_geo = screen.width()
+                self.pos = "right"
 
-            return width, pos
-        return None
+        if self.pos == "left":
+            self.setGeometry(-30, -30, 100, 100)
+            rotation = 180
+        else:
+            rotation = 0
+            self.setGeometry(self.width_geo - 80, -30, 100, 100)
+
+        pixmap = self.original_pixmap.transformed(QTransform().rotate(rotation))
+        self.image_label.setPixmap(pixmap)
