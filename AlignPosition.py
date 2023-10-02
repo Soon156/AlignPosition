7061ecc72efce4c69c1056afcf5fea00412d2d49
soonk <soonkokseng2015@gmail.com>
2023-10-02 23:44:55 +0800

    now accept argument to run app in background

--- AlignPosition.py
+++ AlignPosition.py
@@ -1,33 +1,47 @@
 import sys
+import argparse
 from PySide6 import QtWidgets
-from Funtionality.Config import check_key
+from Funtionality.Config import check_key, check_logo
 from Funtionality.ErrorMessage import WarningMessageBox
 from Funtionality.UpdateConfig import tracking_app_use_time
 from Window.main import MainWindow
-from Funtionality import Config
+from Funtionality.Config import get_config, check_process, clear_log
+
+
+def main(background):
+    values = get_config()
+
+    # Create the application instance
+    app = QtWidgets.QApplication(sys.argv)
+
+    if check_logo():
+        if not check_process():
+            get_config()
+            clear_log()
+
+            if values['app_tracking'] == "True" and check_key():
+                tracking_app_use_time()
+
+            window = MainWindow(background)
+            window.setScreen(app.primaryScreen())
+
+            # Start the event loop
+            app.exec()
+        else:
+            title = "Warning"
+            hint = "The 'Align Position' process is already running."
+            error = "Application Running"
+            widget = WarningMessageBox(title, hint, error)
+    else:
+        title = "Error"
+        hint = "Missing Icon Resources, reinstall the application"
+        error = "File Not Found"
+        widget = WarningMessageBox(title, hint, error)
+
 
 if __name__ == '__main__':
-    values = Config.get_config()
-    if not Config.check_process():
-        # Create the application instance
-        app = QtWidgets.QApplication(sys.argv)
+    parser = argparse.ArgumentParser(description="Enhanced Parental Control and Posture Monitoring Application")
+    parser.add_argument("--background", action="store_true", help="Run the application in the background")
 
-        Config.get_config()
-        Config.clear_log()
-
-        if values['app_tracking'] == "True" and check_key():
-            tracking_app_use_time()
-
-        # Create the main window
-        window = MainWindow()
-        window.setScreen(app.primaryScreen())
-        window.show()
-
-        # Start the event loop
-        app.exec()
-    else:
-        app = QtWidgets.QApplication(sys.argv)
-        title = "Warning"
-        hint = "The 'Align Position' process is already running."
-        error = "Application Running"
-        widget = WarningMessageBox(title, hint, error)
+    args = parser.parse_args()
+    main(args.background)
