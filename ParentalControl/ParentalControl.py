d2024ffedea194c6d0e2ac8400eae591ed7210f0
soonk <soonkokseng2015@gmail.com>
2023-10-23 22:47:25 +0800

    update parental control

--- ParentalControl/ParentalControl.py
+++ ParentalControl/ParentalControl.py
@@ -3,7 +3,7 @@ import time
 import logging as log
 from datetime import datetime
 from PySide6.QtCore import QThread, Signal
-from Funtionality.Config import get_config # parental_monitoring
+from Funtionality.Config import get_config  # parental_monitoring
 from Funtionality.Notification import show_control, reset_signal, get_signal
 from ParentalControl.Auth import read_table_data
 
@@ -20,7 +20,7 @@ class ParentalTracking(QThread):
         self.data = read_table_data()
         self.state = False  # To control the notification not spam
         self.total_time_state = False  # To control the total time notification
-        self.start_time = time.time()  # Time to reset notification state
+        self.notify_time = 0  # Time to reset notification state
         self.thread = None
         # parental_monitoring(value=1)
 
@@ -33,12 +33,20 @@ class ParentalTracking(QThread):
         self.total_time_state = False
         self.state = False
 
+    def update_time(self):
+        if self.current_day != datetime.now().weekday():
+            self.total_time_state = False
+            self.current_day = datetime.now().weekday()
+        self.current_time = datetime.now().time()
+
     def run(self):
         log.info("Parental tracking start")
         while self.cond_usetime:
             time.sleep(1)
-            # Check total use time
-            limit_time = self.data[0]
+            self.update_time()
+            # TODO Check total use time
+
+            limit_time = self.data[0][self.current_day]
             limit_time_in_sec = limit_time * 60 * 60
             try:
                 use_time = self.parent().latest_usetime
@@ -51,15 +59,16 @@ class ParentalTracking(QThread):
                 self.thread = threading.Thread(target=show_control)
                 self.thread.start()
                 self.total_time_state = True
+
             # Check the computer access time
-            if (self.current_day, self.current_time.hour) in self.data[2:] and not self.state:
+            if (self.current_day, self.current_time.hour) in self.data[2] and not self.state:
                 self.thread = threading.Thread(target=show_control)
                 self.thread.start()
+                self.notify_time = time.time()
                 self.state = True
 
             # Re-enable the notification after an hour
-            if time.time() - self.start_time > 3600:
-                self.start_time = time.time()
+            if time.time() - self.notify_time > 3600:
                 self.state = False
 
             cancel_signal = get_signal()
