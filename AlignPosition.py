c1830157fa253d7ed9304af4833bb8af7500ab29
soonk <soonkokseng2015@gmail.com>
2023-10-25 21:24:52 +0800

    working loading screen

--- AlignPosition.py
+++ AlignPosition.py
@@ -1,18 +1,65 @@
 import sys
 import argparse
-from PySide6 import QtWidgets
 
+from PySide6.QtWidgets import QApplication
 from Window.loadingWindow import GifAnimationDialog
+import resource_rc
 
 
 def main(background):
-
     # Create the application instance
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     w = GifAnimationDialog()
-    w.setScreen(app.primaryScreen())
-    w.check_background(background)
+
+    if not background:
+        w.show()
+
+    check_condition()
+
+    from Window.main import MainWindow
+    window = MainWindow()
+    window.setScreen(app.primaryScreen())
+
+    if background:
+        window.hide()
+    else:
+        window.show()
+
+    w.finish(window)
     sys.exit(app.exec())
+
+
+def check_condition():
+    from Funtionality.Config import get_config, check_model, check_process, check_logo, retrieve_filter, clear_log, \
+        check_key
+    from Funtionality.ErrorMessage import WarningMessageBox
+    from Funtionality.UpdateConfig import tracking_app_use_time
+    values = get_config()
+    if check_logo() and check_model():
+        if not check_process():
+            try:
+                get_config()
+                retrieve_filter()
+                clear_log()
+            except Exception as e:  # warning when file in use
+                title = "Warning"
+                hint = str(e)
+                error = "Something Wrong"
+                WarningMessageBox(title, hint, error)
+
+            if values['app_tracking'] == "True" and check_key():
+                tracking_app_use_time()
+
+        else:
+            title = "Warning"
+            hint = "The 'Align Position' process is already running."
+            error = "Application Running"
+            WarningMessageBox(title, hint, error)
+    else:
+        title = "Error"
+        hint = "Missing Critical Resources, please reinstall the application"
+        error = "File Not Found"
+        WarningMessageBox(title, hint, error)
 
 
 if __name__ == '__main__':
--- Window/loadingWindow.py
+++ Window/loadingWindow.py
@@ -1,15 +1,9 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QMovie, QPixmap
-from PySide6.QtWidgets import QDialog, QLabel, QVBoxLayout
-
-from Funtionality.Config import check_logo, check_model, check_process, clear_log, check_key, get_config, \
-    retrieve_filter
-from Funtionality.ErrorMessage import WarningMessageBox
-from Funtionality.UpdateConfig import tracking_app_use_time
-from Window.main import MainWindow
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QSplashScreen
 
 
-class GifAnimationDialog(QDialog):
+class GifAnimationDialog(QSplashScreen):
 
     def __init__(self):
         super().__init__()
@@ -40,48 +34,3 @@ class GifAnimationDialog(QDialog):
         self.layout.setAlignment(Qt.AlignCenter)
         self.layout.addWidget(self.title_label)
         self.layout.addWidget(self.gif_label)
-        self.background = False
-        self.show()
-        self.check_condition()
-
-    def check_condition(self):
-
-        values = get_config()
-        if check_logo() and check_model():
-            if not check_process():
-                try:
-                    get_config()
-                    retrieve_filter()
-                    clear_log()
-                except Exception as e:  # warning when file in use
-                    title = "Warning"
-                    hint = str(e)
-                    error = "Something Wrong"
-                    WarningMessageBox(title, hint, error)
-
-                if values['app_tracking'] == "True" and check_key():
-                    tracking_app_use_time()
-
-                window = MainWindow()
-                window.setScreen(self.screen())
-
-                if self.background:
-                    window.hide()
-                else:
-                    window.show()
-
-            else:
-                title = "Warning"
-                hint = "The 'Align Position' process is already running."
-                error = "Application Running"
-                WarningMessageBox(title, hint, error)
-        else:
-            title = "Error"
-            hint = "Missing Critical Resources, please reinstall the application"
-            error = "File Not Found"
-            WarningMessageBox(title, hint, error)
-
-        self.close()
-
-    def check_background(self, background):
-        self.background = background
