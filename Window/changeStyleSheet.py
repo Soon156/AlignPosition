cde30516fbaa08009ee4f3c859cf8f1a92f51789
soonk <soonkokseng2015@gmail.com>
2023-11-15 03:01:41 +0800

    add theme control on chart

--- Window/changeStyleSheet.py
+++ Window/changeStyleSheet.py
@@ -1,4 +1,6 @@
-from Funtionality.Config import get_config
+from matplotlib import pyplot as plt
+
+from Funtionality.Config import get_config, GRAY_COLOR
 
 # Side Menu stylesheet reset
 top_side_menu = "background: #164863;border-top-left-radius: 25px;border-top-right-radius: 25px;"
@@ -14,6 +16,10 @@ def get_theme():
     values = get_config()
     index = values['theme']
     if index == "0":
+        plt.rcParams['text.color'] = 'white'
+        plt.rcParams['axes.labelcolor'] = 'white'
         return False
     else:
+        plt.rcParams['text.color'] = GRAY_COLOR
+        plt.rcParams['axes.labelcolor'] = GRAY_COLOR
         return True
